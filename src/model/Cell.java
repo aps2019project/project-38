f36f87910cc41315619457ee3476ab672db3ad6d
ali <aj.ali.javan@gmail.com>
2019-04-15 10:47:05 +0430

    small change.

--- src/model/Cell.java
+++ src/model/Cell.java
@@ -1,7 +1,6 @@
 package model;
 
 import model.cards.warriors.Warrior;
-import model.effects.Effect;
 import model.triggers.Trigger;
 
 import java.util.ArrayList;
@@ -9,5 +8,4 @@ import java.util.ArrayList;
 public class Cell {
     private Warrior warrior;
     ArrayList<Trigger> triggers = new ArrayList<>();
-    ArrayList<Effect> effects = new ArrayList<>();
 }
