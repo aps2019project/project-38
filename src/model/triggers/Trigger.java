442063211a895ad56de22e1ba1860501b27a38fa
amir <amirmoieni@outlook.com>
2019-04-30 00:13:49 +0430

    cleaned up trigger from removed features(i.e. owner)

--- src/model/triggers/Trigger.java
+++ src/model/triggers/Trigger.java
@@ -9,9 +9,6 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 public abstract class Trigger extends QualityHaver implements Serializable {
-    //used for adding triggers stored in a trigger to another warrior because their owners should be set again.
-//    private Warrior warrior;
-//    private Cell cell;
     ArrayList<Condition> conditions = new ArrayList<>();
     int duration;
     Dispelablity dispelablity;
@@ -20,14 +17,6 @@ public abstract class Trigger extends QualityHaver implements Serializable {
         this.dispelablity = dispelablity;
         this.duration = duration;
     }
-
-//    public Warrior getWarrior() {
-//        return warrior;
-//    }
-//
-//    public Cell getCell() {
-//        return cell;
-//    }
 
     public ArrayList<Condition> getConditions() {
         return conditions;
@@ -38,7 +27,6 @@ public abstract class Trigger extends QualityHaver implements Serializable {
     }
 
     public void check(GameState gameState, QualityHaver owner) {
-//        setOwner(owner);
         for (Condition condition : conditions) {
             if (condition.check(gameState, this,owner)) {
                 return;
@@ -46,17 +34,6 @@ public abstract class Trigger extends QualityHaver implements Serializable {
         }
         apply(gameState,owner);
     }
-
-//    private void setOwner(Object owner) {
-//        cell = null;
-//        warrior = null;
-//
-//        if (owner instanceof Cell) {
-//            cell = (Cell) owner;
-//        } else if (owner instanceof Warrior) {
-//            warrior = (Warrior) owner;
-//        }
-//    }
 
     protected abstract void apply(GameState gameState, QualityHaver owner);
 }
