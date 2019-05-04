334dcb1a0f1371ecb86981d43e43dfb93eaca8d6
ali <aj.ali.javan@gmail.com>
2019-05-04 23:35:15 +0430

    Main created. necessary methods(runLastWindow, openWindow, closeWindow) added to Window.

--- src/controller/window/Window.java
+++ src/controller/window/Window.java
@@ -1,5 +1,25 @@
 package controller.window;
 
+import java.util.ArrayList;
+
 public abstract class Window {
+    private static ArrayList<Window> openWindows = new ArrayList<>();
+
+    public static boolean runLastOpenWindow() {
+        if (openWindows.size() > 0) {
+            openWindows.get(openWindows.size() - 1);
+            return true;
+        }
+        return false;
+    }
+
+    public static void openWindow(Window window) {
+        openWindows.add(window);
+    }
+
+    public static void closeWindow(Window window) {
+        openWindows.remove(window);
+    }
+
     public abstract void main();
 }
