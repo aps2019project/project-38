24cfffe4cf06f6cb017014253566a68ff0288df7
AliJavanJ1 <aj.ali.javan@gmail.com>
2019-07-09 11:45:20 +0430

    Auctions created.

---
+++ client/view/fxmlControllers/AuctionsController.java
@@ -0,0 +1,24 @@
+package view.fxmlControllers;
+
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+
+public class AuctionsController {
+    public VBox myAuctionsLeftVBox;
+    public VBox myAuctionsMiddleVBox;
+    public VBox myAuctionsRightVBox;
+    public VBox otherAuctionsLeftVBox;
+    public VBox otherAuctionsMiddleVBox;
+    public VBox otherAuctionsRightVBox;
+    public ImageView backButton;
+
+    public void back(MouseEvent mouseEvent) {
+    }
+
+    public void shineBackBottom(MouseEvent mouseEvent) {
+    }
+
+    public void resetBackBottom(MouseEvent mouseEvent) {
+    }
+}
