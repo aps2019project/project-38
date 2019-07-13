1621aca2e21f014fcfab34d60161f0c5bc9e7855
AliJavanJ1 <aj.ali.javan@gmail.com>
2019-07-13 12:54:57 +0430

    nothing.

--- server/server/ServerInit.java
+++ server/server/ServerInit.java
@@ -8,6 +8,7 @@ import model.Shop;
 import server.net.ServerSession;
 import view.Loader;
 import view.WindowChanger;
+import view.fxmlControllers.ShopController;
 import view.fxmls.LoadedScenes;
 
 import java.io.IOException;
@@ -47,7 +48,9 @@ public class ServerInit extends Application {
             System.exit(0);
         });
 
+        ShopController.shopController.calculateEverything();
         WindowChanger.instance.setMainParent(LoadedScenes.shop);
+
 
         mainStage.show();
     }
